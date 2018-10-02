6cdc8c8e39e3f2f1a8b09c2482a72deccc85252c
Julian-Hirasawa <julian_perezcanillas@outlook.es>
2018-10-02 09:03:48 +0200

    Añadidos ejemplos Tema1

---
+++ Tema1/SalidaFormateada01.java
@@ -0,0 +1,7 @@
+public class SalidaFormateada01 {
+  public static void main(String[] args) {
+    System.out.printf("El numero %d no tiene decimales . \n", 21);
+    System.out.printf("El numero %f sale con decimales. \n", 21.0);
+    System.out.printf("El %.6f sale exactamente con tres decimales. \n", 69.0);
+  }
+}
